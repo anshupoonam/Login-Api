e3f4ecb6eeb4c3ca0a52ced222eae41765e0ae62
Poonam Yadav <88348179+anshupoonam@users.noreply.github.com>
2021-08-05 03:04:21 +0530

    Update api_service.dart
    Api 415 issue solved

--- api/api_service.dart
+++ api/api_service.dart
@@ -6,16 +6,23 @@ import 'dart:async';
 import 'package:milkmanagement/model/login_model.dart';
 
 class APIService {
-
   Future<LoginResponseModel> login(LoginRequestModel requestModel) async {
-   var url = Uri.parse("http://api.erp9i.com/api/auth/login");
-    final response = await http.post(url, body: requestModel.toJson());
-     print(response);
+    var url = Uri.parse("http://api.erp9i.com/api/auth/login");
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    final response = await http.post(url,
+        body: jsonEncode(requestModel.toJson()), headers: requestHeaders);
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${requestModel.toJson()}');
     if (response.statusCode == 200 || response.statusCode == 400) {
       return LoginResponseModel.fromJson(
         json.decode(response.body),
       );
-    } else {
+    }
+    else {
       throw Exception('Failed to load data!');
     }
   }
